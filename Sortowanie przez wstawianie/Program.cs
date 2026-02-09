809a0e056d0729df4118a33643598721549e289f
Mango67 <4422807@TE-GDA-D-D21E03>
2026-02-09 12:41:40 +0100

    Jednak zły algorytm

--- Sortowanie przez wstawianie/Program.cs
+++ Sortowanie przez wstawianie/Program.cs
@@ -2,10 +2,10 @@
 
 Random rand = new Random();
 
-int[] Array = new int[rand.Next(5, 150)];
+int[] Array = new int[rand.Next(5, 10)];
 for (int i = 0; i < Array.Length; i++)
 {
-    Array[i] = rand.Next(0, 100);
+    Array[i] = rand.Next(0, 10);
 }
 
 int[] SortedArray = Sorter.Sort(Array);
--- Sortowanie przez wstawianie/Sorter.cs
+++ Sortowanie przez wstawianie/Sorter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,21 +12,21 @@ namespace Sortowanie_przez_wstawianie
         public static int[] Sort(int[] InArray)
         {
             int[] OutArray = new int[InArray.Length];
-
-            for (int j = 0; j < InArray.Length; j++)
+            for (int i = 0; i < OutArray.Length; i++)
             {
-                int smallestIndex = 0;
+                OutArray[i] = int.MaxValue;
+            }
 
-                for (int i = 0; i < InArray.Length; i++)
+            OutArray[0] = InArray[0];
+            for (int i = 1; i < InArray.Length - 1; i++)
+            {
+                for (int j = i; j < InArray.Length - 1; j++)
                 {
-                    if (InArray[i] < InArray[smallestIndex])
+                    if (InArray[i] < OutArray[j])
                     {
-                        smallestIndex = i;
+                        OutArray[j - 1] = InArray[i];
                     }
                 }
-
-                OutArray[j] = InArray[smallestIndex];
-                InArray[smallestIndex] = int.MaxValue;
             }
 
             return OutArray;
