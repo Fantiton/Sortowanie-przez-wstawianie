08f71bae73464e11fefe21c4e4fc31054c124355
Mango67 <4422807@TE-GDA-D-D21E03>
2026-02-09 11:34:20 +0100

    Dodaj pliki projektów.

---
+++ Sortowanie przez wstawianie/Program.cs
@@ -0,0 +1,12 @@
+﻿using Sortowanie_przez_wstawianie;
+
+Random rand = new Random();
+
+int[] Array = new int[rand.Next(5, 150)];
+for (int i = 0; i < Array.Length; i++)
+{
+    Array[i] = rand.Next(0, 100);
+}
+
+int[] SortedArray = Sorter.Sort(Array);
+Console.WriteLine(string.Join(", ", SortedArray));
---
+++ Sortowanie przez wstawianie/Sorter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortowanie_przez_wstawianie
+{
+    public static class Sorter
+    {
+        public static int[] Sort(int[] InArray)
+        {
+            int[] OutArray = new int[InArray.Length];
+
+            for (int j = 0; j < InArray.Length; j++)
+            {
+                int smallestIndex = 0;
+
+                for (int i = 0; i < InArray.Length; i++)
+                {
+                    if (InArray[i] < InArray[smallestIndex])
+                    {
+                        smallestIndex = i;
+                    }
+                }
+
+                OutArray[j] = InArray[smallestIndex];
+                InArray[smallestIndex] = int.MaxValue;
+            }
+
+            return OutArray;
+        }
+    }
+}
